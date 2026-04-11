4d636d2ba343aee81abba026dee31af5d731f001
pavanidodla <dodlapavani511@gmail.com>
2026-04-11 09:42:13 +0530

    updated

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api',
+  baseURL:
+    process.env.REACT_APP_API_URL ||
+    'https://faculty-feedback-d38s.onrender.com/api',
 });
 
 API.interceptors.request.use((config) => {
